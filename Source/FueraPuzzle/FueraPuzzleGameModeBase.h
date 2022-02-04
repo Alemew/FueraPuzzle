65df417a26393bad8e6adfb853b357e77f6a40c7
Alemew <aleorperez@hotmail.com>
2022-02-04 19:00:14 +0100

    inicio

---
+++ Source/FueraPuzzle/FueraPuzzleGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FueraPuzzleGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FUERAPUZZLE_API AFueraPuzzleGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
